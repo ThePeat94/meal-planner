ab989ef37825ad1575d2c9ba106d8f9f007c1509
Hendrik Rakemann <dev@hendrikrakemann.de>
2023-12-28 00:44:53 +0100

    Install Skeleton UI Library. Includes TailwindCSS.

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,26 +1,39 @@
 <script lang="ts">
-    import { browser } from '$app/environment'
-    import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
-    import { SvelteQueryDevtools } from '@tanstack/svelte-query-devtools';
-    import axios from 'axios';
+	import '../app.pcss';
+	import { browser } from '$app/environment';
+	import { QueryClient, QueryClientProvider } from '@tanstack/svelte-query';
+	import { SvelteQueryDevtools } from '@tanstack/svelte-query-devtools';
+    import { AppShell } from '@skeletonlabs/skeleton';
+	import axios from 'axios';
 
-    const queryClient = new QueryClient({
-        defaultOptions: {
-            queries: {
-                enabled: browser,
-            },
-        },
-    })
-  
-    axios.defaults.baseURL = 'localhost:5190';
-    axios.defaults.headers.post['Content-Type'] = 'application/json';
-    axios.defaults.headers.post['Accept'] = 'application/json';
-    axios.defaults.headers.get['Accept'] = 'application/json';
-    axios.defaults.headers.get['Content-Type'] = 'application/json';
+	const queryClient = new QueryClient({
+		defaultOptions: {
+			queries: {
+				enabled: browser
+			}
+		}
+	});
+
+	axios.defaults.baseURL = 'localhost:5190';
+	axios.defaults.headers.post['Content-Type'] = 'application/json';
+	axios.defaults.headers.post['Accept'] = 'application/json';
+	axios.defaults.headers.get['Accept'] = 'application/json';
+	axios.defaults.headers.get['Content-Type'] = 'application/json';
 </script>
 
-
-<QueryClientProvider client="{queryClient}">
-    <slot/>
-    <SvelteQueryDevtools/>
+<QueryClientProvider client={queryClient}>
+    <AppShell slotSidebarLeft="bg-surface-500/5 w-56 p-4">
+        <svelte:fragment slot="sidebarLeft">
+            <!-- Insert the list: -->
+            <nav class="list-nav">
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                </ul>
+            </nav>
+            <!-- --- -->
+        </svelte:fragment>
+        <slot/>
+    </AppShell>
+	<SvelteQueryDevtools/>
 </QueryClientProvider>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,5 +1,11 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="container mx-auto p-8 space-y-8">
+	<h1 class="h1">Hello Skeleton</h1>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+	<section>
+		<a class="btn variant-filled-primary" href="https://kit.svelte.dev/">SvelteKit</a>
+		<a class="btn variant-filled-secondary" href="https://tailwindcss.com/">Tailwind</a>
+		<a class="btn variant-filled-tertiary" href="https://github.com/">GitHub</a>
 
-hehe hello world xD
-omfg this is working
+        <button type="button" class="btn variant-filled-primary">Button</button>
+	</section>
+</div>
