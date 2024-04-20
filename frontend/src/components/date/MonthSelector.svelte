d9b7b0d54e62bff4db972269f550a12317ed3694
Hendrik Rakemann <dev@hendrikrakemann.de>
2024-04-20 08:09:27 +0200

    Introduce Month and Year Selection for MealPlans

---
+++ frontend/src/components/date/MonthSelector.svelte
@@ -0,0 +1,59 @@
+<script lang="ts">
+	import Select from 'components/Select.svelte';
+	import type { SelectOption } from 'components/types/SelectOption';
+
+	export let value: number;
+
+	const monthOptions: SelectOption<number>[] = [
+		{
+			label: 'January',
+			value: 0,
+		},
+		{
+			label: 'February',
+			value: 1,
+		},
+		{
+			label: 'March',
+			value: 2,
+		},
+		{
+			label: 'April',
+			value: 3,
+		},
+		{
+			label: 'May',
+			value: 4,
+		},
+		{
+			label: 'June',
+			value: 5,
+		},
+		{
+			label: 'July',
+			value: 6,
+		},
+		{
+			label: 'August',
+			value: 7,
+		},
+		{
+			label: 'September',
+			value: 8,
+		},
+		{
+			label: 'October',
+			value: 9,
+		},
+		{
+			label: 'November',
+			value: 10,
+		},
+		{
+			label: 'December',
+			value: 11,
+		},
+	];
+</script>
+
+<Select label="Month" options={monthOptions} bind:value />
--- frontend/src/routes/mealplans/+page.svelte
+++ frontend/src/routes/mealplans/+page.svelte
@@ -4,6 +4,7 @@
 	import PrimaryButton from 'components/buttons/PrimaryButton.svelte';
 	import { format, getDaysInMonth, isSameDay } from 'date-fns';
 	import { sortAsc, sortByDate } from 'utils/sortUtils';
+	import MonthSelector from 'components/date/MonthSelector.svelte';
 
 	const modalStore = getModalStore();
 
@@ -36,39 +37,55 @@
 	};
 
 	const currentDate = new Date();
-	const dayCount = getDaysInMonth(currentDate);
+
+	let selectedMonth = currentDate.getMonth();
+	let selectedYear = currentDate.getFullYear();
+
+	$: selectedDate = new Date(selectedYear, selectedMonth);
+	$: dayCount = getDaysInMonth(selectedDate);
 </script>
 
 <AppBar class="mb-4">
 	<svelte:fragment slot="lead"><h4 class="h4">Meal Plan</h4></svelte:fragment>
+	<svelte:fragment>
+		<div class="grid grid-cols-2 grid-rows-1 gap-2">
+			<MonthSelector bind:value={selectedMonth} />
+			<div class="">
+				<label class="label">
+					<span>Year</span>
+					<input type="number" class="input rounded-lg p-1.5" bind:value={selectedYear} />
+				</label>
+			</div>
+		</div>
+	</svelte:fragment>
 	<svelte:fragment slot="trail">
 		<PrimaryButton text="Add Meal" onClick={handleDialogOpen} />
 	</svelte:fragment>
 </AppBar>
 
-{#if $meals.isSuccess && !$meals.isRefetching}
-	<div>
-		<div class="grid grid-cols-7 gap-1">
-			{#each Array(dayCount) as _, day}
-				<div
-					class="z-0 h-40 rounded-lg bg-emerald-100 transition-colors hover:bg-emerald-500"
-					role="cell"
-					tabindex={day + 1}
-					on:click={() => handleDialogOpenForDay(day + 1)}
-					on:keydown={(k) => {
-						if (k.code === 'Enter') {
-							handleDialogOpenForDay(day + 1);
-							return;
-						}
-					}}
-				>
-					<div class="grid grid-cols-1 gap-1 p-2">
-						<div>
-							{day + 1}
-						</div>
+<div>
+	<div class="grid grid-cols-7 gap-1">
+		{#each Array(dayCount) as _, day}
+			<div
+				class="z-0 h-40 rounded-lg bg-emerald-100 transition-colors hover:bg-emerald-500 dark:bg-emerald-900 dark:hover:bg-emerald-700"
+				role="cell"
+				tabindex={day + 1}
+				on:click={() => handleDialogOpenForDay(day + 1)}
+				on:keydown={(k) => {
+					if (k.code === 'Enter') {
+						handleDialogOpenForDay(day + 1);
+						return;
+					}
+				}}
+			>
+				<div class="grid grid-cols-1 gap-1 p-2">
+					<div>
+						{day + 1}
+					</div>
+					{#if $meals.isSuccess && !$meals.isRefetching}
 						{#each getMealsForDay(day + 1) as meal}
 							<div
-								class="z-10 rounded-lg bg-sky-300 p-1 transition-colors hover:bg-sky-500"
+								class="z-10 rounded-lg bg-sky-300 p-1 transition-colors hover:bg-sky-500 dark:bg-sky-600"
 								role="cell"
 								tabindex={day + 1}
 								on:click={(mouseEvent) => mouseEvent.stopPropagation()}
@@ -78,9 +95,9 @@
 								{format(meal.at, 'HH:mm')}
 							</div>
 						{/each}
-					</div>
+					{/if}
 				</div>
-			{/each}
-		</div>
+			</div>
+		{/each}
 	</div>
-{/if}
+</div>
